5b4c9416b38145ed5a16b286f6afcccd38fa7272
cahittkc <66890236+cahittkc@users.noreply.github.com>
2025-07-25 04:16:31 +0300

    ss

--- pages/customers/index.vue
+++ pages/customers/index.vue
@@ -1,9 +1,72 @@
 <template>
-    <div class="minh bg-gradient-to-b from-gray-900 to-black py-10 px-4 max-w-7xl">
-        <div class="flex w-full justify-end items-center">
+    <div class=" py-10 px-4 max-w-7xl minh">
+        <div class="flex w-full justify-end items-center mb-4">
             <button @click="openModal" class="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg ml-auto">Yeni Müvekkil</button>
         </div> 
-    </div>
+    
+        <div>
+            <div class="flex items-center gap-x-5 p-3 rounded-2xl shadow-xl bg-gray-900 mb-4">
+                <div class="flex flex-col gap-y-1">
+                    <span class="text-gray-300 text-sm">Müvekkil Tipi</span>
+                    <select v-model="customerListObj.customerType" @change="getCustomers()" class="input-item min-w-[231px]">
+                        <option value="person">Kişi</option>
+                        <option value="company">Şirket</option>
+                    </select>
+                </div>
+            </div>
+
+
+            <div v-if="selectedCustomerType === 'person'" class="overflow-x-auto rounded-2xl">
+                <table class="min-w-full w-full divide-y divide-gray-800">
+                    <thead class="bg-gray-900">
+                        <tr>
+                            <th class="px-4 py-3 text-left text-xs font-bold text-gray-200 tracking-wider uppercase">Tc Kimlik No</th>
+                            <th class="px-4 py-3 text-left text-xs font-bold text-gray-200 tracking-wider uppercase">AD soyad</th>
+                            <th class="px-4 py-3 text-left text-xs font-bold text-gray-200 tracking-wider uppercase">Olusturma Tarihi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="(customer, index) in customers"
+                            :key="customer.id"
+                            :class="(index % 2 === 0) ? 'bg-gray-800' : 'bg-gray-900'"
+                        >
+                            <td class="px-4 py-3 text-gray-100 font-medium whitespace-nowrap">{{ customer.identityNumber }}</td>
+                            <td class="px-4 py-3 text-gray-100 font-medium whitespace-nowrap">{{ customer.firstName }} {{ customer.middleName }} {{ customer.lastName }}</td>
+                            <td class="px-4 py-3 text-gray-100">
+                                {{ moment(customer.createdAt).format('DD/MM/YYYY HH:mm:ss') }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div v-if="selectedCustomerType === 'company'" class="overflow-x-auto rounded-2xl">
+                <table class="min-w-full w-full divide-y divide-gray-800">
+                    <thead class="bg-gray-900">
+                        <tr>
+                            <th class="px-4 py-3 text-left text-xs font-bold text-gray-200 tracking-wider uppercase">Vergi No</th>
+                            <th class="px-4 py-3 text-left text-xs font-bold text-gray-200 tracking-wider uppercase">Şirket/Kurum adı</th>
+                            <th class="px-4 py-3 text-left text-xs font-bold text-gray-200 tracking-wider uppercase">Olusturma Tarihi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            v-for="(customer, index) in customers"
+                            :key="customer.id"
+                            :class="(index % 2 === 0) ? 'bg-gray-800' : 'bg-gray-900'"
+                        >
+                            <td class="px-4 py-3 text-gray-100 font-medium whitespace-nowrap">{{ customer.taxNumber }}</td>
+                            <td class="px-4 py-3 text-gray-100 font-medium whitespace-nowrap">{{ customer.companyName }}</td>
+                            <td class="px-4 py-3 text-gray-100">
+                                {{ moment(customer.createdAt).format('DD/MM/YYYY HH:mm:ss') }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>    
+    </div>    
 
 
     <generalModal v-model="addCustomerModal" title="Müvekkil Ekle">
@@ -11,7 +74,7 @@
             <div class="flex flex-col gap-y-3">
                 <div class="flex flex-col gap-y-1.5">
                     <label class="xs-txt" for="name">Müvekkil Tipi</label>
-                    <select v-model="newCustomerObj.customerType" class="input-item">
+                    <select v-model="newCustomerObj.customerType" class="input-item min-w-[231px]">
                             <option value="person">Kişi</option>
                             <option value="company">Şirket</option>
                     </select>
@@ -50,17 +113,40 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref,computed } from 'vue';
 import { useAuthStore } from '@/stores/auth';
+import moment from 'moment';
 
 definePageMeta({
     layout: 'auth'
 })
 
+interface customer {
+    id: string,
+    firstName: string,
+    middleName: string | null,
+    lastName: string,
+    companyName: string | null,
+    customerType: string,
+    identityNumber: string | null,
+    taxNumber: string | null,
+    caseNumbers: string[],
+    createdAt: string,
+    deletedAt: string | null,
+    updatedAt: string | null
+}
+
 const authStore = useAuthStore()
 
 
 const addCustomerModal = ref(false)
+const customerListObj = ref({
+    userId : authStore?.authInfo?.id,
+    page : 1,
+    pageSize : 15,
+    customerType : 'person'
+})
+const customers = ref<customer[]>([])
 
 const newCustomerObj = ref({
     userId : authStore?.authInfo?.id,
@@ -72,6 +158,10 @@ const newCustomerObj = ref({
     identityNumber : null as string | null,
     taxNumber : null as string | null,
     
+})
+
+const selectedCustomerType = computed(() => {
+    return customerListObj.value.customerType
 })
 
 
@@ -117,8 +207,21 @@ const addCustomer = async () => {
     const response = await apiRequest('post', '/customers/add-customer', newCustomerObj.value)
     console.log(response.data);
     if(response.data.success){
+        getCustomers()
         closeModal()
     }
 }
+
+const getCustomers = async () => {
+
+    const response = await apiRequest('post', '/customers/get-customers', customerListObj.value)
+    if(response.data.success){
+        customers.value = response.data.data
+    }
+}
+
+onMounted(async () => {
+    getCustomers()
+})
 
 </script>
